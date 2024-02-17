b4032b01a558195d1d9f103271f02b33083c31f8
Najaf <nacaf0832@gmail.com>
2024-02-17 12:30:09 -0800

    mini_project

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log(2 * 4 === 8);
+//1
+let numberOfFilms = +prompt("Skolko filmov vi uje posmotreli", "");
+console.log(numberOfFilms);
+
+let a = prompt("Odin iz poslednix prosmotrennix filmov?"),
+  b = prompt("Na skolko ocenite ego?"),
+  c = prompt("Odin iz poslednix prosmotrennix filmov?"),
+  d = prompt("Na skolko ocenite ego?");
+
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
